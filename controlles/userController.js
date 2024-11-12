6c069c147fc4710012dee4a27d2fc8d369b67767
Abril Guzman Barrera <230350@utxicotepec.edu.mx>
2024-11-12 14:28:55 -0600

    Practica12
    Se agrego las notificaciones d eerror en los campos y validacion

--- controlles/userController.js
+++ controlles/userController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from 'express-validator'//para detonar validaciones y el otro para darnos la validacion 
 import Usuario from '../models/Usuario.js'
 
 const formularioLogin = (req,res) => {
@@ -16,6 +17,25 @@ const formularioRegister = (req,res) => {
 }
 //async : estan haciendose varias cosas al mismo tiempo, se divide el trabajo 
 const registrar= async(req,res)=> {
+
+    //validacion 
+    await check('nombre').notEmpty().withMessage('El nombre no puede ir vacio').run(req)//para verificar si el campo n o esta vacio 
+    await check('email').isEmail().withMessage('Eso no parece un email').run(req)
+    await check('password').isLength({min:6}).withMessage('El password debe de ser por lo menos de seis caracteres').run(req)
+    await check('validacion_contraseña').equals('password').withMessage('El password no coincide').run(req)
+
+
+    //return res.json(resultado.array())
+    let resultado = validationResult(req)
+    //verificar si el resultado esta vacio
+    if(resultado.isEmpty()){
+        return res.render('auth/register', {
+            page: "Crea una cuenta...",
+            errores: resultado.array()
+    
+        })
+    }
+
    const usuario = await Usuario.create(req.body)
    console.log(req.body)
    res.json(usuario)//retorna el usuario con informacion en la base de datos 
